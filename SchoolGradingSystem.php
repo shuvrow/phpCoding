8714f5fcffdc10c3a93af8a639b69e4dccf7f116
shuvrow <shuvrow@gmail.com>
2014-07-09 15:07:43 +0600

    Looping and Finding summetion of number 0 to 9

--- SchoolGradingSystem.php
+++ SchoolGradingSystem.php
@@ -3,56 +3,28 @@
 $student[1]=78;
 $student[2]=93;
 $student[3]=24;
-if(80 <= $student[2] && 100 >= $student[2])
+if($student[1]>79)
 {
-	echo 'A+';
-	echo "<br/>";
+	echo "A+";
 }
 
-
-elseif (70 <= $student[2] && 79 >= $student[2])
+if($student[1]>79)
 {
-	echo 'A';
-	echo "<br/>";
+	echo "A+";
 }
 
-elseif(60 <= $student[2] && 69 >= $student[2])
+if($student[1]>79)
 {
-	echo 'A-';
-	echo "<br/>";
+	echo "A+";
 }
 
-
-elseif(50 <= $student[2] && 59 >= $student[2])
+if($student[1]>79)
 {
-	echo 'b';
-	echo "<br/>";
+	echo "A+";
 }
 
-
-elseif(40 <= $student[2] && 49 >= $student[2])
+if($student[1]>79)
 {
-	echo 'c';
-	echo "<br/>";
+	echo "A+";
 }
-
-
-elseif(33 <= $student[2] && 39 >= $student[2])
-{
-	echo 'd';
-	echo "<br/>";
-}
-
-elseif(0 <= $student[2] && 32 >= $student[2])
-{
-	echo 'd';
-	echo "<br/>";
-}
-else
-{
-echo 'Invalid Number';
-
-}
-
-
 ?>
