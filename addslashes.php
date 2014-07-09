8fbf49ff9e0d9fa239411a506fb511eee30275af
shuvrow <shuvrow@gmail.com>
2014-07-09 23:54:30 +0600

    Add Slashes 2nd program

--- addslashes.php
+++ addslashes.php
@@ -1,8 +1,17 @@
 
 <?php
-$str = "Is your name O'reilly?";
-
+$str1 = "Is your name O'reilly?";
+$str2 = "Is your name O|reilly?";
+$str3 = "Is your name O\reilly?";
+$str4 = "Is your name O\freilly?";
 
 // Outputs: Is your name O\'reilly?
-echo addslashes($str);
+echo addslashes($str1);
+echo "<br/>";
+echo addslashes($str2);
+echo "<br/>";
+echo addslashes($str3);
+echo "<br/>";
+echo addslashes($str4);
+
 ?>
