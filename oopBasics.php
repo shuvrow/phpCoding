878123e0b869fd569062be0ab2ae58792f12c871
hasan doha <doha.physics.du@gmail.com>
2014-07-10 12:41:46 +0600

    Accessing a class properties

--- oopBasics.php
+++ oopBasics.php
@@ -2,14 +2,14 @@
 //creating a new class User
 class User
 {
-$name="";
-$email="";
-
-};
+var $name="FTFL";
+var $email="";
+}
 //instances of User
 $user1=new User();
 
 
-var_dump($user1);
+//var_dump($user1);
 
+echo $user1->name;
 ?>
