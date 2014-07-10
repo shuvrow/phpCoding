2d83f2bf37aac7c9bff0ead9edc816b3c200b059
hasan doha <doha.physics.du@gmail.com>
2014-07-10 10:59:47 +0600

    first edition

---
+++ functionTestexercise.php
@@ -0,0 +1,21 @@
+<?php
+$fields = array
+(
+	"genres" => array("comedy","tragedy","action","romance"),
+	"film-titles"=>array("Big","Star Wars","Titanic","Freanch Kiss"),
+	"Stars"=> array("Bill Murray","Mark Hammella", "Leonard Dicaprio",
+		"cate Blanchett"),
+	);
+
+
+
+
+function collectinformation($name)
+{
+
+
+}
+$a = collectinformation(foreach ($fields as  $value) {
+	
+}
+?>
