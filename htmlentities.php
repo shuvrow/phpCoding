94c6f1f0ef84a6a6ca7100bb719c543e57145cee
shuvrow <shuvrow@gmail.com>
2014-07-11 23:57:49 +0600

    difference between htmlEntities and html entiry Decode

--- htmlentities.php
+++ htmlentities.php
@@ -1,8 +1,11 @@
 
 <?php
-$str = "A 'quote' is <b>bold</b>";
+$str = "A 'quote' is <i>bold</i>";
+$str1= "I'll \"walk\" the <b>dog</b> now";
 
 // Outputs: A 'quote' is &lt;b&gt;bold&lt;/b&gt;
 echo htmlentities($str);
+echo "<br/>";
+echo htmlentities($str1);
 
 ?>
