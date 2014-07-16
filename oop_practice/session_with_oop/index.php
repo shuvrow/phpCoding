0c1dd255b9e696e6a1f76f7f36ff988992c2d850
shuvrow <shuvrow@gmail.com>
2014-07-17 00:38:48 +0600

    Sample php program using both OOP and Session : Adding students to a department

---
+++ oop_practice/session_with_oop/index.php
@@ -0,0 +1,10 @@
+<html>
+<head>
+	</head>
+	<body>
+		<a href="departmentinfo.php" >Department</a><br/>
+		<a href="studentinfo.php">Student</a><br/>
+		<a href="show.php">View Information</a>
+	</body>
+</html>
+
